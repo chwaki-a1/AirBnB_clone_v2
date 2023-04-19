c5cd711a6f772ddc3e0eca50797892a766f84807
chwaki <chwakik@gmail.com>
2023-04-19 14:15:55 +0000

    clone

--- models/user.py
+++ models/user.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+'''
+    Implementation of the User class which inherits from BaseModel
+'''
 from models.base_model import BaseModel, Base
-from models import storage_type
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    if storage_type == 'db':
+    '''
+        Definition of the User class
+    '''
+    __tablename__ = "users"
+    if getenv("new_TYPE_STORAGE") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
+
+        places = relationship('Place', cascade='all, delete-orphan',
+                              backref='user')
+        reviews = relationship('Review', cascade='all, delete-orphan',
+                               backref='user')
     else:
         email = ""
         password = ""
